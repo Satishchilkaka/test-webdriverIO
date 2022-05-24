dc7972d88abbf4a5dcba915eeec0fee6118319b9
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2022-05-23 22:01:33 -0400

    add sample components

--- ang-concepts/src/app/app.module.ts
+++ ang-concepts/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BaseComponent } from './base/base.component';
+import { InOutComponent } from './base/in-out/in-out.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BaseComponent,
+    InOutComponent
   ],
   imports: [
     BrowserModule
