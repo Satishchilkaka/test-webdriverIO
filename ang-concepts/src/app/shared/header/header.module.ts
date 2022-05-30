61141e8eb2885b494396b5a2e3b231e39282ac7c
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2022-05-29 22:44:40 -0400

    add shared module

---
+++ ang-concepts/src/app/shared/header/header.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header.component';
+import { BaseComponent } from 'src/app/base/base.component';
+import { NameComponent } from 'src/app/base/in-out/input/name/name.component';
+
+
+
+
+const appRoutes: Routes = [
+  { path: 'inputOutput', component: BaseComponent },
+
+
+]
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
+  ],
+  exports: [
+    HeaderComponent
+  ],
+  declarations: [HeaderComponent]
+})
+export class HeaderModule { }
+
+
